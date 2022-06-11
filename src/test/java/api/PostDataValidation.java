aed82b737641c5ea7d2ffc8bd6699cda8be77ea6
NTS-AmitKorade <amitark1995@gmail.com>
2022-06-11 16:30:56 +0530

    Add post Method

--- src/test/java/api/PostDataValidation.java
+++ src/test/java/api/PostDataValidation.java
@@ -3,30 +3,22 @@ package api;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ApiUtils;
-
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class PostDataValidation {
     @Test(priority = 1)
     public void postRequest() throws IOException {
         ApiUtils.readAPIConfigs();
         ApiUtils.setup();
-        RequestSpecification httprequest= RestAssured.given();
-        JSONObject requestparam=new JSONObject();
-        requestparam.put("employee_name","Amit");
-        requestparam.put("employee_salary","50000");
-        requestparam.put("employee_age","50");
-        requestparam.put("profile_image","");
-        httprequest.header("Content-Type","application/json");
-        httprequest.body(requestparam.toJSONString());
+        String body = Files.readString(Path.of(System.getProperty("user.dir") + "/src/test/java/api_test_data/CreateUserJsonBody.json"));
         String end_point=ApiUtils.api_config.getProperty("create_user_end_point");
-        Response response=httprequest.post(end_point,requestparam);
-        String responseBoday = response.getBody().asString();
+        Response response = ApiUtils.post(end_point, body);
         Assert.assertEquals(response.statusCode(),201,"status code mismatch");
-        System.out.println(responseBoday);
+
     }
 }
--- src/test/java/utilities/ApiUtils.java
+++ src/test/java/utilities/ApiUtils.java
@@ -1,7 +1,10 @@
 package utilities;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.internal.RequestSpecificationImpl;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,6 +12,8 @@ import java.util.Properties;
 
 public class ApiUtils {
    public static Properties api_config;
+    public static Response response;
+    public static RequestSpecification request;
  public static void readAPIConfigs() throws IOException {
      api_config = new Properties();
      FileInputStream prop = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\java\\resourse\\api_config.properties");
@@ -19,4 +24,13 @@ public class ApiUtils {
         RestAssured.baseURI = api_config.getProperty("base_url");
     }
 
+    public static Response post(String boday,String endpoint)
+    {
+        RequestSpecification httprequest= RestAssured.given();
+        httprequest.header("Accept","application/json");
+        httprequest.header("Content-Type","application/json");
+        httprequest.body(boday);
+        response=httprequest.post(endpoint);
+        return response;
+    }
 }
