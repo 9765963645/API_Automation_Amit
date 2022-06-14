75ee2d399e2533b75343a2181efb59cb8e0ad5a0
NTS-AmitKorade <amitark1995@gmail.com>
2022-06-14 08:42:22 +0530

    commit

--- src/test/java/api/PostDataValidation.java
+++ src/test/java/api/PostDataValidation.java
@@ -7,6 +7,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.ApiUtils;
 import java.io.IOException;
@@ -14,18 +15,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class PostDataValidation {
+    @BeforeClass
+    public void setup() throws IOException {
+        ApiUtils.readAPIConfigs();
+        ApiUtils.setBaseURL();
+    }
     public static Response response;
     @Feature("API Test")
     @Story("API-001: post Request")
     @Description("to test new user is created when create user api is called")
     @Test(priority = 1,description = "to verify new user is created when create user api is called")
     public void postRequest() throws IOException {
-        ApiUtils.readAPIConfigs();
-        ApiUtils.setBaseURL();
         String body = Files.readString(Path.of(System.getProperty("user.dir") + "/src/test/java/api_test_data/CreateUserBody.json"));
-        String end_point=ApiUtils.api_config.getProperty("/users");
-         response = ApiUtils.postRequest(end_point,body);
+         response = ApiUtils.postRequest("/users",body);
         Assert.assertEquals(response.statusCode(),201,"status code mismatch");
-
     }
 }
